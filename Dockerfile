9b02639b0867a6b26a210fd9a8a4f1d548427077
Adrian Roque <aroque@c1r3p2.42sp.org.br>
2020-03-15 14:45:58 -0300

    Add server autoindex switch feature

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,16 @@ COPY srcs/phpMyAdmin-5.0.1-all-languages.tar /tmp/
 COPY srcs/wordpress-5.3.2.tar.gz /tmp/
 COPY srcs/certificate_authority.sh /tmp/
 COPY srcs/domains.ext /tmp/
-COPY srcs/mysql_install.sh /tmp/
+COPY srcs/autoindex.sh .
 COPY srcs/setup.sh /tmp/
 COPY srcs/services.sh /tmp/
 
-# SysAdmin Credentials. Can be passed as argument at build time.
+# SysAdmin credentials and environment
+# variables. Can be passed as arguments
+# at build time.
 ARG USER=cadet
 ARG PASSWD=42saopaulo
-ARG ROOT_PASSWD=root
+ENV AUTO_INDEX on
 
 RUN apt-get update && apt-get install -y \
 	apt-utils gnupg \
---
+++ srcs/autoindex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Quick script to set the autoindex function
+# of a NGINX server config file using sed.
+# The index [on|off] variable can be passed
+# as an argument or given by the AUTO_INDEX
+# environment variable. This variable is changed
+# in current shell session if the script is run
+# in the calling shell context.
+# e.g. "source /path/to/autoindex.sh"
+# e.g. ". /path/to/autoindex.sh"
+
+INDEX=${1:-$AUTO_INDEX}
+CONFIG_FILE=/etc/nginx/sites-enabled/ft_server.conf
+
+if [[ "$INDEX" == "off" || "$INDEX" == "on" ]];
+then
+	sed -i -E "/autoindex/ s/on|off/$INDEX/" $CONFIG_FILE
+	export AUTO_INDEX=$INDEX
+else
+	echo "Variable not accepted. Insert 'on' or 'off'"
+fi
